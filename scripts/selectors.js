937468d46e4e24744c21949c223e34a7f6625ee6
Mantas Lingaitis <mantasling@gmail.com>
2022-11-17 21:10:53 +0200

    > Project file setup

---
+++ scripts/selectors.js
@@ -0,0 +1,8 @@
+/* 
+>> Documentation for JavaScript
+    [] document object model
+    [] selectors
+>> Page: 34
+>> Link to doc: https://docs.google.com/document/d/1NoFPu4Z9_kNf3hDG59YhuLVXH1JKc71kgO7CtFawtDw/edit?skip_itp2_check=true#
+*/
+
