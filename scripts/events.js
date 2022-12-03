8a3ddb4f4beb326551db0b00ebcfd4ac5a088485
Mantas Lingaitis <mantasling@gmail.com>
2022-12-03 09:59:01 +0200

    > Item list toggle done functionality

--- scripts/events.js
+++ scripts/events.js
@@ -1,10 +1,12 @@
 // https://developer.mozilla.org/en-US/docs/Web/Events
 // https://developer.mozilla.org/en-US/docs/Web/Events
 
-// ADD item to list section
+// >>> ADD item to list section
 var listTextInput = document.getElementById("userInput");
 var listButton = document.getElementById("addToList");
 var itemList = document.getElementById("itemList");
+var items = document.getElementsByClassName("newItem")
+
 
 function GetListInputLength() {
     return listTextInput.value.length;
@@ -12,11 +14,21 @@ function GetListInputLength() {
 
 function AddItemToList() {
     var listItem = document.createElement("li");
+    var listDeleteButton = document.createElement("button");
+
+    listItem.className = "newItem"
+    listDeleteButton.className = "newItemDelete"
+
+    listDeleteButton.appendChild(document.createTextNode("X"));
+    listItem.appendChild(listDeleteButton);
 
     listItem.appendChild(document.createTextNode(listTextInput.value));
     itemList.appendChild(listItem);
+}
 
-    listTextInput.value = "";
+
+function toggleDoneItems() {
+    items.add("done");
 }
 
 function AddToListWithKeypress(event) {
@@ -26,18 +38,31 @@ function AddToListWithKeypress(event) {
 }
 
 function AddToListWithClick() {
-        if (GetListInputLength() > 0) {
-            AddItemToList();
-        }
+    if (GetListInputLength() > 0) {
+        AddItemToList();
+    }
 }
 
+function resolveClickActions(element) {
+    setItemsDone(element);
+}
+
+function setItemsDone(element) {
+    if (element.target.tagName === "LI") {
+        element.target.classList.toggle("done");
+    }
+}
+
+itemList.addEventListener("click", resolveClickActions)
 listButton.addEventListener("click", AddToListWithClick)
 listTextInput.addEventListener("keypress", AddToListWithKeypress)
 
-// Testing some events with buttons section
+
+// >>> Testing some events with buttons section
 var clickBtn = document.getElementById("btn1");
 var enterBtn = document.getElementById("btn2");
 var exitBtn = document.getElementById("btn3");
+
 
 function testMouseEvents(button, eventType) {
     button.addEventListener(eventType, function () {
@@ -45,7 +70,6 @@ function testMouseEvents(button, eventType) {
     })
 }
 
-testMouseEvents(clickBtn, "click")
-testMouseEvents(enterBtn, "mouseenter")
-testMouseEvents(exitBtn, "mouseleave")
-
+testMouseEvents(clickBtn, "click");
+testMouseEvents(enterBtn, "mouseenter");
+testMouseEvents(exitBtn, "mouseleave");
