4f9fd0b552f2793e6c9434a9add54cbe618f0399
Mantas Lingaitis <mantasling@gmail.com>
2022-12-04 18:21:15 +0200

    > Gradient random color generator button

--- scripts/gradientBackground.js
+++ scripts/gradientBackground.js
@@ -1,4 +1,4 @@
-function getBackgroundColorValues(backgroundColors) {
+function getBackgroundColorValuesText(backgroundColors) {
     var currentColorText = document.querySelector(".text_content h2");
 
     currentColorText.textContent = "Current background colors:" + backgroundColors.style.background;
@@ -9,17 +9,31 @@ function setGradient() {
     var backgroundBlock = document.getElementById("background_canvas_block");
     backgroundBlock.style.background = "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
 
-    getBackgroundColorValues(backgroundBlock)
+    getBackgroundColorValuesText(backgroundBlock)
 }
 
-function changeColorBackgroundOnColorInput(eventType, color) {
-    color.addEventListener(eventType, setGradient)
+function calculateRandomColorString() {
+    return "#" + ((1 << 24) * Math.random() | 0).toString(16).padStart(6, "0");
+}
+
+function setRandomGradientColor() {
+    color1.value = calculateRandomColorString();
+    color2.value = calculateRandomColorString();
+
+    setGradient();
+}
+
+function changeColorBackgroundOnColorInput(eventType, inputType, gradientFunction) {
+    inputType.addEventListener(eventType, gradientFunction)
 }
 
 var color1 = document.getElementById("color1");
 var color2 = document.getElementById("color2");
+var randomColorButton = document.getElementById("randomColorGeneratorButton");
 
-changeColorBackgroundOnColorInput("input", color1);
-changeColorBackgroundOnColorInput("input", color2);
-
+// Set starting colors and color text on page load
 setGradient()
+
+changeColorBackgroundOnColorInput("input", color1, setGradient);
+changeColorBackgroundOnColorInput("input", color2, setGradient);
+changeColorBackgroundOnColorInput("click", randomColorButton, setRandomGradientColor);
